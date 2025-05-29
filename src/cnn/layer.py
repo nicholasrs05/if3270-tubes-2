e514dd1de473665020b5c9e215abef90227a739b
Nicholas Sihite <nicholassihite05@gmail.com>
2025-05-29 13:20:16 +0700

    chore: add comments for clarity

--- src/cnn/layer.py
+++ src/cnn/layer.py
@@ -66,16 +66,16 @@ class Conv2DScratch(LayerScratch):
         if self.padding == 'same':
             pad_h = ((h - 1) * sh + kh - h) // 2
             pad_w = ((w - 1) * sw + kw - w) // 2
-            input = np.pad(input, ((0,0), (pad_h, pad_h), (pad_w, pad_w), (0,0)), mode='constant')
+            input = np.pad(input, ((0,0), (pad_h, pad_h), (pad_w, pad_w), (0,0)), mode='constant') # ngasih padding
 
-        out_h = (input.shape[1] - kh) // sh + 1
+        out_h = (input.shape[1] - kh) // sh + 1 # ini basically V = (W - F + 2P) / S + 1, tpi 2P-nya udh ditambahin waktu cek padding
         out_w = (input.shape[2] - kw) // sw + 1
 
-        cols = image_to_col(input, kh, kw, sh, sw, out_h, out_w)
+        cols = image_to_col(input, kh, kw, sh, sw, out_h, out_w) # gambarnya dijadiin kolom spy komputasinya cepat
         w_col = self.weights.reshape(-1, self.filters)
 
-        out = np.matmul(cols, w_col) + self.bias
-        out = out.reshape(batch, out_h, out_w, self.filters)
+        out = np.matmul(cols, w_col) + self.bias # kernel * input + bias
+        out = out.reshape(batch, out_h, out_w, self.filters) # ubah spy V * V * k
 
         if self.activation == 'relu':
             out = np.maximum(0, out)
@@ -116,17 +116,17 @@ class MaxPooling2DScratch(LayerScratch):
             out_h = (h - ph) // sh + 1
             out_w = (w - pw) // sw + 1
 
-        output = np.zeros((batch, out_h, out_w, c))
+        output = np.zeros((batch, out_h, out_w, c)) # nyiapin tempat output
 
         for y in range(out_h):
             for x in range(out_w):
-                region = input[:, y*sh:y*sh+ph, x*sw:x*sw+pw, :]
-                output[:, y, x, :] = np.max(region, axis=(1, 2))
+                region = input[:, y*sh:y*sh+ph, x*sw:x*sw+pw, :] # ini ambil area buat di-pool
+                output[:, y, x, :] = np.max(region, axis=(1, 2)) # max dari areanya
 
         return output
 
 
-class AvgPooling2DScratch(LayerScratch):
+class AvgPooling2DScratch(LayerScratch): # basically sama kayak MaxPooling2D, cuma ganti max jadi mean
     def __init__(self, pool_size=(2, 2), strides=None, padding='valid'):
         super().__init__()
         self.pool_size = pool_size
@@ -197,7 +197,7 @@ class DenseScratch(LayerScratch):
         if self.weights.shape[0] != input.shape[1]:
             raise ValueError(f"Weights shape {self.weights.shape[0]} doesn't match input shape {input.shape[1]}")
         
-        output = input @ self.weights + self.bias
+        output = input @ self.weights + self.bias # udh berbentuk vektor, jadi tinggal dot product sama weights + bias
         if self.activation == 'relu':
             output = np.maximum(0, output)
         elif self.activation == 'sigmoid':
